90323e6949d2be684b2c793686d4dbb89232959b
Rengoan <91058472+Rengoan@users.noreply.github.com>
2021-10-15 11:34:27 +0200

    1234

--- AritmeticaExcepcion/src/aritmetica/Aritmetica.java
+++ AritmeticaExcepcion/src/aritmetica/Aritmetica.java
@@ -4,7 +4,7 @@ import excepciones.OperacionesExcepcion;
 
 public class Aritmetica {
 
-    public static int division(int numerador, int denominador)throws OperacionesExcepcion{
+    public static int division(int numerador, int denominador) throws OperacionesExcepcion {
         if (denominador == 0) {
             throw new OperacionesExcepcion("Division entre 0");
         }
--- AritmeticaExcepcion/src/test/Test.java
+++ AritmeticaExcepcion/src/test/Test.java
@@ -1,18 +1,16 @@
-
 package test;
 
 import static aritmetica.Aritmetica.division;
 
-
 public class Test {
-    
+
     public static void main(String[] args) {
-        int res= 0;
-        
-        try{
-            res = division(10,0);
-        
-        } catch (Exception e){
+        int res = 0;
+
+        try {
+            res = division(10, 0);
+
+        } catch (Exception e) {
             e.printStackTrace(System.out);
         }
     }
